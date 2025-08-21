5641cddf822ae45484572e40e3d99ab431fe4d9a
thiagoefonseca <thiagoefonseca2007@gmail.com>
2025-08-20 21:53:33 -0300

    Adição da parte do usuário

--- obj/Debug/net8.0-maccatalyst/maccatalyst-x64/FlowyAPP.AssemblyInfo.cs
+++ obj/Debug/net8.0-maccatalyst/maccatalyst-x64/FlowyAPP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlowyAPP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a4b6dd2792cac9347b8731de9ebd1785f3374e22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+e0f134db1c57fe51cd1b962cd94ba56c1f5e68c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlowyAPP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlowyAPP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
